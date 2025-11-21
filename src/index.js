cae73502a8b768b7dce942b368706b4c7c4ba434
snowparsley <dhfpswlthd1234@naver.com>
2025-11-22 01:39:30 +0900

    plus

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <AuthProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </AuthProvider>
 );
